6aeb7e1067537bd1becac2590c6c9b856f89b6b2
arunphilip03 <philip03@gmail.com>
2014-02-27 18:59:45 +0530

    View, UIShell, Model changes

---
+++ ViewController/src/com/sfa/view/backing/ConfirmBean.java
@@ -0,0 +1,28 @@
+package com.sfa.view.backing;
+
+
+import oracle.adf.model.BindingContext;
+import oracle.adf.view.rich.event.DialogEvent;
+
+import oracle.binding.BindingContainer;
+import oracle.binding.OperationBinding;
+
+public class ConfirmBean {
+    public ConfirmBean() {
+    }
+    
+    
+    public void dialogListener(DialogEvent dialogEvent) {
+        
+        if(dialogEvent.getOutcome().equals(DialogEvent.Outcome.ok)) {
+            BindingContainer bc = BindingContext.getCurrent().getCurrentBindingsEntry();
+            OperationBinding opDelete = bc.getOperationBinding("Delete"); 
+            OperationBinding opCommit = bc.getOperationBinding("Commit"); 
+            opDelete.execute();
+            opCommit.execute();
+            
+        
+        }
+    }
+    
+}
---
+++ ViewController/src/com/sfa/view/backing/RefreshView.java
@@ -0,0 +1,39 @@
+package com.sfa.view.backing;
+
+import oracle.adf.model.BindingContext;
+import oracle.adf.model.binding.DCBindingContainer;
+import oracle.adf.model.binding.DCIteratorBinding;
+
+import org.apache.myfaces.trinidad.context.RequestContext;
+import org.apache.myfaces.trinidad.event.ReturnEvent;
+
+public class RefreshView {
+    public RefreshView() {
+    }
+    
+    
+    public void contactDialogReturnListener(ReturnEvent returnEvent) {
+        BindingContext bc = BindingContext.getCurrent();
+        
+        DCBindingContainer dcb = (DCBindingContainer)bc.getCurrentBindingsEntry();
+        DCIteratorBinding iter = dcb.findIteratorBinding("CustomerContactsView2Iterator");
+        
+        iter.executeQuery();
+        
+        RequestContext.getCurrentInstance().addPartialTarget(returnEvent.getComponent().getParent().getParent());
+            
+    }
+    
+    
+    public void customerDialogReturnListener(ReturnEvent returnEvent) {
+        BindingContext bc = BindingContext.getCurrent();
+        
+        DCBindingContainer dcb = (DCBindingContainer)bc.getCurrentBindingsEntry();
+        DCIteratorBinding iter = dcb.findIteratorBinding("CustomerAccountView1Iterator");
+        
+        iter.executeQuery();
+        
+        RequestContext.getCurrentInstance().addPartialTarget(returnEvent.getComponent().getParent().getParent());
+            
+    }
+}
